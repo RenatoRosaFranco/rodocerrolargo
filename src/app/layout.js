563d410b9749313a4b6baea32f8a9a441bd0620a
Renato Rosa Franco <renato_ny@live.com>
2025-07-28 13:17:40 -0300

    fix: fixing minor import error.

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,10 +8,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Geist, Geist_Mono } from "next/font/google";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-import "./globals.css";
+import Header from '@/ui/components/Header';
+import Footer from '@/ui/components/Footer';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,11 @@ import { Container, Row, Col } from 'react-bootstrap';
 export default function Home() {
   return (
     <>
-      <Container className="mt-5">
-        <Row className="mt-5">
-          <Col className="text-center" md={12}>
-            <h1>Em construção</h1>
-            <p>Estamos trabalhando para trazer a melhor experiência possível para você.</p>
+      <Container className="py-5">
+        <Row className="py-5">
+          <Col className="text-center py-5" md={12}>
+            <h1 className="fw-bold">Em construção</h1>
+            <p>Estamos trabalhando para trazer a melhor<br /> experiência possível para você.</p>
           </Col>
         </Row>
       </Container>
