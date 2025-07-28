b423192a7b61f9856873c9f5a4420539c0357d68
Renato Rosa Franco <renato_ny@live.com>
2025-07-28 14:43:31 -0300

    feature: adding sitemap configuration.

---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: 'https://rodocerrolargo.com.br',
+  generateRobotsTxt: true,
+}
---
+++ src/ui/components/Faq.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Faq = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Faq;
